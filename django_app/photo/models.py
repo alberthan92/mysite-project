4662d2aaae79aca10c090266100491040caf4064
alberthan92 <alberthan92@gmail.com>
2016-10-25 16:32:06 +0900

    Create Album and Photo in the models of Photo app.

--- django_app/photo/models.py
+++ django_app/photo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Album(models.Model):
+    title = models.CharField(max_length=30)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+    description = models.CharField(max_length=800, blank=True)
+
+
+class Photo(models.Model):
+    album = models.ForeignKey(Album)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=80, blank=True)
+    img = models.ImageField(upload_to='photo')
