9b16d378e67b54c99b038b2585326c4f1d5f85c4
alberthan92 <alberthan92@gmail.com>
2016-10-28 00:05:23 +0900

    Deleted the views.py within the photo app.

--- django_app/photo/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from photo.models import Album
-
-def album_list(request):
-    albums = Album.objects.all()
-    return render(request, 'photo/album_list.html', {'album_list': albums})
