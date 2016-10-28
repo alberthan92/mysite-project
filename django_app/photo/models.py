01dc3d453797efdfd36ab4ff6f8bc45936c7c1f9
alberthan92 <alberthan92@gmail.com>
2016-10-28 12:52:35 +0900

    Added a function that makes the input image into a thumbnail image. Also an image thumbnail field is added to the photo function.

--- django_app/photo/models.py
+++ django_app/photo/models.py
@@ -13,8 +13,58 @@ class Photo(models.Model):
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=80, blank=True)
     img = models.ImageField(upload_to='photo')
+    img_thumbnail = models.ImageField(upload_to='photo/thumbnail', blank=True)
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, through='PhotoLike', related_name='photo_set_like_users')
     dislike_users = models.ManyToManyField(settings.AUTH_USER_MODEL, through='PhotoDislike', related_name='photo_set_dislike_users')
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        image_changed = False
+
+        if self.pk:
+            ori = Photo.objects.get(pk=self.pk)
+            if ori.img != self.img:
+                image_changed = True
+
+        if self.img and not self.img_thumbnail:
+            image_changed = True
+
+        super().save(*args, **kwargs)
+        if image_changed:
+            self.make_thumbnail()
+
+    def make_thumbnail(self):
+        import os
+        from PIL import Image, ImageOps
+        from io import BytesIO
+        from django.core.files.base import ContentFile
+        from django.core.files.storage import default_storage
+
+        size = (300, 300)
+        f = default_storage.open(self.img)
+        image = Image.open(f)
+        ftype = image.format
+
+        image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+        path, ext = os.path.splitext(self.img.name)
+        name = os.path.basename(path)
+
+        thumbnail_name = '%s_thumb%s' % (name, ext)
+
+        temp_file = BytesIO()
+        image.save(temp_file, ftype)
+        temp_file.seek(0)
+
+        content_file = ContentFile(temp_file.read())
+        self.img_thumbnail.save(thumbnail_name, content_file)
+
+        temp_file.close()
+        content_file.close()
+        f.close()
+        return True
 
 
 class PhotoLike(models.Model):
