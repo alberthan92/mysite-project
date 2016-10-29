90a753b4a3121f3a191345b00408552d378c6296
alberthan92 <alberthan92@gmail.com>
2016-10-29 21:43:07 +0900

    Added url pattern for ajax photo like.

--- django_app/photo/urls.py
+++ django_app/photo/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
+from . import ajax
 
 urlpatterns = [
     url(r'^$', views.album_list, name='album_list'),
@@ -7,4 +8,5 @@ urlpatterns = [
     url(r'^album/(?P<pk>[0-9]+)/$', views.album_detail, name='album_detail'),
     url(r'^album/(?P<album_pk>[0-9]+)/photo/add/$', views.photo_add, name='photo_add'),
     url(r'^photo/(?P<pk>[0-9]+)/(?P<like_type>\w+)/$', views.photo_like, name='photo_like'),
+    url(r'^ajax/photo/(?P<pk>[0-9]+)/(?P<like_type>\w+)/$', ajax.photo, name='ajax_photo_like'),
 ]
